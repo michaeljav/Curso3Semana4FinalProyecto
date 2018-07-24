fc449da6484afb6861ab84391de202e457bf7ecf
mjavier <michaeljaviermota@gmail.com>
2018-07-24 12:35:04 -0400

    primera subida

---
+++ app/src/main/java/com/exampleandroiddemottest/petagramrecyclerview/CreateMenuOpctions.java
@@ -0,0 +1,37 @@
+package com.exampleandroiddemottest.petagramrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+
+public class CreateMenuOpctions extends AppCompatActivity {
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//       getMenuInflater().inflate(R.menu.menu_opciones,menu);
+//
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        switch (item.getItemId()){
+//
+//            case R.id.mAbout:
+//                Toast.makeText(getApplicationContext(),"abourt",Toast.LENGTH_SHORT).show();
+//                break;
+//
+//
+//            case  R.id.mContact:
+//                Toast.makeText(getApplicationContext(),"contactos",Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+//
+//        return true;
+//    }
+}
---
+++ app/src/main/java/com/exampleandroiddemottest/petagramrecyclerview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.exampleandroiddemottest.petagramrecyclerview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends CreateMenuOpctions {
+
+    ArrayList<Pets> pets;
+    private ImageButton SegundaPantalla;
+    private RecyclerView recyclerViewlistaPets;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.miActionBar);
+        setSupportActionBar(toolbar);
+
+        SegundaPantalla = findViewById(R.id.SegundaPantalla);
+
+        SegundaPantalla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"click",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,DetallePets.class);
+                startActivity(intent);
+            }
+        });
+
+        recyclerViewlistaPets = findViewById(R.id.rvPets);
+
+        //Forma en mostrar  el en el recycler;
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerViewlistaPets.setLayoutManager(llm);
+
+
+        llenarRecycler llenarRecycler = new llenarRecycler();
+       pets=llenarRecycler.inicializarListadePets();
+        llenarRecycler.inicializarAdactador(pets,this,recyclerViewlistaPets);
+
+
+
+    }
+
+
+}
